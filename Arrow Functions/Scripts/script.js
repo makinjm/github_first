e964ca19d4a46b414c967a298b6962df6f0a58f6
makinjm <makinjm@gmail.com>
2018-06-27 22:22:19 +0100

    Arrow Functions

--- Arrow Functions/Scripts/script.js
+++ Arrow Functions/Scripts/script.js
@@ -9,3 +9,12 @@ console.log(getPrice2(2));
 
 var getPrice3 = (count, tax) => count * 4.00 * (1 + tax);
 console.log(getPrice3(2, .07));
+
+var getPrice4 = (count, tax) => {
+    var price = count * 4.00;
+    price *= (1 + tax);
+    return price;
+}
+document.getElementById("output").innerHTML = "Result= " + getPrice4(2, .07);
+
+document.addEventListener('click', () => document.getElementById("Thisoutput").innerHTML = "This = " + this);
